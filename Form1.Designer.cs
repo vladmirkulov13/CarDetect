15b44046b2d6574a0c0ba83e05ef83e0db8f95b4
vladmirkulov13 <72435641+vladmirkulov13@users.noreply.github.com>
2021-08-06 12:32:17 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CarDetect
+
+{
+    partial class FormForDetectingCars
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormForDetectingCars));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ToolStripMenuItemForChooseFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemForOpenDirections = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonForActivateDetecting = new System.Windows.Forms.ToolStripButton();
+            this.pictureBoxForImageOrVideo = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBoxForResult = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForImageOrVideo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemForChooseFile});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // ToolStripMenuItemForChooseFile
+            // 
+            this.ToolStripMenuItemForChooseFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemForOpenDirections});
+            this.ToolStripMenuItemForChooseFile.Name = "ToolStripMenuItemForChooseFile";
+            resources.ApplyResources(this.ToolStripMenuItemForChooseFile, "ToolStripMenuItemForChooseFile");
+            this.ToolStripMenuItemForChooseFile.Click += new System.EventHandler(this.ToolStripMenuItemForChooseFile_Click);
+            // 
+            // ToolStripMenuItemForOpenDirections
+            // 
+            this.ToolStripMenuItemForOpenDirections.Name = "ToolStripMenuItemForOpenDirections";
+            resources.ApplyResources(this.ToolStripMenuItemForOpenDirections, "ToolStripMenuItemForOpenDirections");
+            this.ToolStripMenuItemForOpenDirections.Click += new System.EventHandler(this.открытьToolStripMenuMenuItem_click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonForActivateDetecting});
+            resources.ApplyResources(this.toolStrip1, "toolStrip1");
+            this.toolStrip1.Name = "toolStrip1";
+            // 
+            // toolStripButtonForActivateDetecting
+            // 
+            this.toolStripButtonForActivateDetecting.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            resources.ApplyResources(this.toolStripButtonForActivateDetecting, "toolStripButtonForActivateDetecting");
+            this.toolStripButtonForActivateDetecting.Name = "toolStripButtonForActivateDetecting";
+            this.toolStripButtonForActivateDetecting.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // pictureBoxForImageOrVideo
+            // 
+            resources.ApplyResources(this.pictureBoxForImageOrVideo, "pictureBoxForImageOrVideo");
+            this.pictureBoxForImageOrVideo.Name = "pictureBoxForImageOrVideo";
+            this.pictureBoxForImageOrVideo.TabStop = false;
+            this.pictureBoxForImageOrVideo.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            resources.ApplyResources(this.openFileDialog1, "openFileDialog1");
+            // 
+            // richTextBox
+            // 
+            resources.ApplyResources(this.richTextBox, "richTextBox");
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.ReadOnly = true;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog1";
+            resources.ApplyResources(this.openFileDialog2, "openFileDialog2");
+            // 
+            // pictureBoxForResult
+            // 
+            resources.ApplyResources(this.pictureBoxForResult, "pictureBoxForResult");
+            this.pictureBoxForResult.Name = "pictureBoxForResult";
+            this.pictureBoxForResult.TabStop = false;
+            // 
+            // FormForDetectingCars
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBoxForResult);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.pictureBoxForImageOrVideo);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormForDetectingCars";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForImageOrVideo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemForChooseFile;
+        private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemForOpenDirections;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonForActivateDetecting;
+        private System.Windows.Forms.PictureBox pictureBoxForImageOrVideo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.PictureBox pictureBoxForResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,458 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Alturos.Yolo;
+using Alturos.Yolo.Model;
+using System.Diagnostics;
+
+
+
+
+namespace CarDetect
+{
+
+
+    public partial class FormForDetectingCars : Form
+    {
+        
+        //создание видеопотока - изображений из видео или камеры
+        
+        Image finalImage = null;
+        YoloWrapper yolo;
+        MemoryStream memoryStream;
+        private int car = 0;
+        private int truck = 0;
+        private int bus = 0;
+        private int motorbike = 0;
+        private int objectCount = 0;
+        private double confCar = 0.0;
+        private double confTruck = 0.0;
+        private double confBus = 0.0;
+        private double confBike = 0.0;
+        private bool noClasses = true;
+        private double conf = 0.0;
+        private String[] types = { "car", "truck", "motorbike" };
+        int maximum = 0;
+        //YoloItem - найденный на изображении объект 
+        //items - для всех объектов
+        //badItems - для объектов, не из области интересов
+        List<YoloItem> badItems = new List<YoloItem>();
+        List<YoloItem> items;
+        private String filename = String.Empty;
+        
+
+        //контруктор общей формы
+        public FormForDetectingCars()
+        {
+            //инициализирует форму и все её составляющие
+            InitializeComponent();
+        }
+        //кнопка перехода к папкам с файлами
+        private void открытьToolStripMenuMenuItem_click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            { 
+                filename = openFileDialog1.FileName;
+                pictureBoxForImageOrVideo.Image = Image.FromFile(filename);
+              
+            }
+            else
+            {
+                MessageBox.Show("Choose the pic", "Picture didn't chose", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+       
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            richTextBox.Clear();
+            pictureBoxForResult.Image = pictureBoxForImageOrVideo.Image;
+            finalImage = pictureBoxForResult.Image;
+
+            //pictureBoxForDetectObjects.Image = pictureBoxForImageOrVideo.Image;
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            //DnnInvoke.ReadNet("yolov3.weights", "yolov3.cfg")
+            yolo = new YoloWrapper("yolov3.cfg", "yolov3.weights", "coco.names");
+
+            memoryStream = new MemoryStream();
+
+            pictureBoxForImageOrVideo.Image.Save(memoryStream, ImageFormat.Jpeg);
+
+            //pictureBoxForDetectObjects.Image.Save(memoryStream, ImageFormat.Jpeg);
+            
+            items = yolo.Detect(memoryStream.ToArray()).ToList<YoloItem>();
+            st.Stop();
+            //finalImage = pictureBoxForDetectObjects.Image;
+
+            Graphics graph = Graphics.FromImage(finalImage);
+            Font font = new Font("Consolas", 20, FontStyle.Bold);
+            SolidBrush brush = new SolidBrush(Color.Black);
+            
+            try
+            {
+                if (pictureBoxForImageOrVideo.Image == null)
+                {
+                    throw new Exception("No image!");
+                }
+
+
+                //pictureBoxForDetectObjects.Image.Save(memoryStream, ImageFormat.Png);
+
+
+                badItems.Clear();
+
+
+                foreach (YoloItem item in items)
+                {
+
+                    /*  double conf = item.Confidence * 100;
+                      if (item.Type != "person" && item.Type != "" )
+                      {*/
+                
+                    conf = item.Confidence * 100;
+                    if (types.Contains(item.Type))
+                    {
+
+
+                        Point rectPoint = new Point(item.X, item.Y);
+                        Size rectSize = new Size(item.Width, item.Height);
+                        Rectangle rect = new Rectangle(rectPoint, rectSize);
+
+                        /*if (conf < 50)
+                        {
+                            Pen pen = new Pen(Color.Red, 5);
+                            graph.DrawRectangle(pen, rect);
+                            //graph.DrawString(item.Type, font, brush, rectPoint);
+                        }
+                        if (conf >= 50 && conf < 80)
+                        {
+                            Pen pen = new Pen(Color.Yellow, 5);
+                            graph.DrawRectangle(pen, rect);
+                            //graph.DrawString(item.Type, font, brush, rectPoint);
+                        }
+                        if (conf > 80)
+                        {
+                            Pen pen = new Pen(Color.Green, 5);
+                            graph.DrawRectangle(pen, rect);
+                            //graph.DrawString(item.Type, font, brush, rectPoint);
+                        }*/
+                        
+
+                        objectCount += 1;
+                        if (conf > 60)
+                        {
+                            switch (item.Type)
+                            {
+                                case "car":
+                                    car += 1;
+                                    confCar += item.Confidence;
+                                    Pen pen = new Pen(Color.Black, 5);
+                                    graph.DrawRectangle(pen, rect);
+                                    break;
+                                case "truck":
+                                    truck += 1;
+                                    confTruck += item.Confidence;
+                                    Pen pen1 = new Pen(Color.Green, 5);
+                                    graph.DrawRectangle(pen1, rect);
+                                    break;
+                                case "bus":
+                                    bus += 1;
+                                    confBus += item.Confidence;
+                                    break;
+                                case "motorbike":
+                                    motorbike += 1;
+                                    confBike += item.Confidence;
+                                    break;
+                            }
+                        }
+                        
+
+                        
+                    }
+
+                }
+                //int[] typesCount = { car, truck, bus, motorbike };
+                /* maximum = typesCount.Max();
+                TextBoxForCount.Text = maximum.ToString();*/
+                //richTextBox.Text += "All types: " + objectCount.ToString() + "\n";
+                richTextBox.Text += "All types: " + (car + truck).ToString() + "\n";
+                if (car != 0)
+                    richTextBox.Text += "Car: " + car.ToString() + "\nAvg conf: " + Math.Floor(confCar / car * 100).ToString() + "%\n";
+                if(truck != 0)
+                    richTextBox.Text += "Truck: " + truck.ToString() + "\nAvg conf: " + Math.Floor(confTruck / truck * 100).ToString() + "%\n";
+                if (bus != 0)
+                    richTextBox.Text += "Bus: " + bus.ToString() + "\nAvg conf: " + Math.Floor(confBus / bus *100).ToString() + "%\n";
+                if (motorbike != 0)
+                    richTextBox.Text += "Bikes: " + motorbike.ToString() + "\nAvg conf: " + Math.Floor(confBike / motorbike *100).ToString() + "%\n";
+
+                confCar = 0;
+                confTruck = 0;
+                confBus = 0;
+                confBike = 0;
+                objectCount = 0;
+                maximum = 0;
+                car = 0;
+                truck = 0;
+                bus = 0;
+                motorbike = 0;
+
+                pictureBoxForResult.Image = finalImage;
+                richTextBox.Text += "Time:" + st.Elapsed.TotalSeconds.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            //pictureBoxForDetectObjects.Image = finalImage;
+        }
+            private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+      
+        /*private void buttonForCameraActivation_Click(object sender, EventArgs e)
+        {
+            String filename = "C://Users//пользователь//Desktop//CCTV Камера наблюдения перекресток Невский проспект и Садовая улица 17feb2021 01-29.mp4";
+            try
+            {
+                capture = new VideoCapture(filename);
+                if (!File.Exists(filename))
+                    throw new Exception("No such file!");
+                
+                capture.ImageGrabbed += Capture_ImageGrabbed;
+
+                capture.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            capture = new VideoCapture();
+
+            
+            capture.ImageGrabbed += Capture_ImageGrabbed;
+
+            capture.Start();
+        }*/
+    
+ 
+
+   
+       
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void ToolStripMenuItemForChooseFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
+// МЕТОДЫ ПОКАЗА БЫВШИХ ПЛОХИХ ОБЪЕКТОВ
+/*try
+            {
+                if (pictureBoxForImageOrVideo.Image == null)
+                {
+                    throw new Exception("No image!");
+                }
+                richTextBoxForRestObjects.Clear();
+                if (badItems.Count != 0)
+                {
+                    if (items.Count - badItems.Count == 0)
+                    {
+                        richTextBoxForRestObjects.Text += "On this photo only :";
+                        foreach (YoloItem item in badItems)
+                        {
+                            richTextBoxForRestObjects.Text += ", " + item.Type.ToString() + " - " + Math.Round((item.Confidence * 100), 2).ToString();
+                        }
+
+                    }
+                    else
+                    {
+                        richTextBoxForRestObjects.Text += "Also here can be";
+                        foreach (YoloItem item in badItems)
+                        {
+                            richTextBoxForRestObjects.Text += ", " + item.Type.ToString() + " - " + Math.Round((item.Confidence * 100), 2).ToString();
+                        }
+                    }
+                }
+                else
+                {
+                    richTextBoxForRestObjects.Text += "No more objects can be here";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }*/
+/*pictureBoxForDetectObjects.Image = pictureBoxForImageOrVideo.Image;
+            finalImage = pictureBoxForDetectObjects.Image;
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            try
+            {
+                if (pictureBoxForImageOrVideo.Image == null)
+                {
+                    throw new Exception("No image!");
+                }
+
+
+                pictureBoxForDetectObjects.Image.Save(memoryStream, ImageFormat.Png);
+
+
+                pictureBoxForImageOrVideo.Image.Save(memoryStream, ImageFormat.Png);
+                Graphics graph = Graphics.FromImage(finalImage);
+                Font font = new Font("Consolas", 20, FontStyle.Bold);
+                SolidBrush brush = new SolidBrush(Color.Black);
+                foreach (YoloItem item in badItems)
+                {
+                    Point rectPoint = new Point(item.X, item.Y);
+                    Size rectSize = new Size(item.Width, item.Height);
+                    Rectangle rect = new Rectangle(rectPoint, rectSize);
+                    Pen pen = new Pen(Color.Black, 5);
+                    if (rect.Width != 0 || rect.Height != 0)
+                        graph.DrawRectangle(pen, rect);
+                    graph.DrawString(item.Type, font, brush, rectPoint);
+
+                }
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception", MessageBoxButtons.OK);
+            }*/
+// МЕТОД ПОИСКА ДОМИНИРУЮЩЕГО ЦВЕТА НА ИЗОБРАЖЕНИИ
+/* public Color getDominantColor(System.Drawing.Bitmap bmp)
+        {
+            BitmapData srcData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            int stride = srcData.Stride;
+
+            IntPtr Scan0 = srcData.Scan0;
+
+            int[] totals = new int[] { 0, 0, 0 };
+
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        for (int color = 0; color < 3; color++)
+                        {
+                            int idx = (y * stride) + x * 4 + color;
+                            totals[color] += p[idx];
+                        }
+                    }
+                }
+            }
+
+            int avgB = totals[0] / (width * height);
+            int avgG = totals[1] / (width * height);
+            int avgR = totals[2] / (width * height);
+
+            bmp.UnlockBits(srcData);
+
+            return Color.FromArgb(avgR, avgG, avgB);
+        }*/
+//ТЯЖЕЛЫЕ РАСЧЕТЫ 
+/*  if (item.Type == "car")
+                              {
+                                  confCar += item.Confidence;
+                                  car += 1;
+                              }
+                              if (item.Type == "truck")
+                              {
+                                  confTruck += item.Confidence;
+                                  truck += 1;
+                              }
+                              if (item.Type == "bus")
+                              {
+                                  confBus += item.Confidence;
+                                  bus += 1;
+                              }
+                          }
+                          if ((item.Confidence * 100) < 80 || item.Type == "person")
+                          {
+                              badItems.Add(item);
+                          }
+
+                      }*/
+/* for (int i = 0; i < items.Count; i++)
+     if (items[i].Type != "person")
+         noClasses = false;
+ if (noClasses == false)
+ {
+     TextBoxForAllCount.Text = items.Count.ToString();
+     int a = Max(car, bus, truck);
+     TextBoxForCount.Text = a.ToString();
+     if (a == car)
+     {
+         TextBoxForType.Text = "car";
+     }
+     if (a == truck)
+     {
+         TextBoxForType.Text = "truck";
+     }
+     if (a == bus)
+     {
+         TextBoxForType.Text = "bus";
+     }
+
+     if (TextBoxForType.Text == "car")
+     {
+         TextBoxForCount.Text = car.ToString();
+         TextBoxForConfidence.Text = (confCar / car).ToString();
+     }
+     if (TextBoxForType.Text == "truck")
+     {
+         TextBoxForCount.Text = truck.ToString();
+         TextBoxForConfidence.Text = (confTruck / truck).ToString();
+     }
+     if (TextBoxForType.Text == "bus")
+     {
+         TextBoxForCount.Text = bus.ToString();
+         TextBoxForConfidence.Text = (confBus / bus).ToString();
+     }
+ }
+ else
+ {
+     TextBoxForType.Text = "No classes";
+     TextBoxForCount.Text = "0";
+     TextBoxForConfidence.Text = "0";
+     TextBoxForAllCount.Text = items.Count.ToString();
+ }*/
+/*Console.WriteLine("GOOD");
+foreach (YoloItem item in items)
+{
+    Console.WriteLine(item.Type + " " + item.Confidence);
+}*/
+/*Console.WriteLine(items.Count);
+Console.WriteLine("BAD");
+foreach (YoloItem item in badItems)
+{
+    Console.WriteLine(item.Type + " " + item.Confidence);
+}
+Console.WriteLine(badItems.Count);*/
